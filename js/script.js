b57aaad55c3c44f5b85c9de6787ead3aa3d4b366
zafer hüzmeli <zafer.huzmeli@iven.io>
2018-05-09 15:49:16 +0300

    edition of kent ajans

--- js/script.js
+++ js/script.js
@@ -1,128 +1,3 @@
-function initMap() {
-    "use strict";
-    var e = new google.maps.Map(document.getElementById("map"),{
-        center: {
-            lat: 47.285339,
-            lng: 9.644274
-        },
-        zoom: 15,
-        styles: [{
-            elementType: "geometry",
-            stylers: [{
-                color: "#272e32"
-            }]
-        }, {
-            elementType: "labels.text.stroke",
-            stylers: [{
-                color: "#272e32"
-            }]
-        }, {
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#403e3e"
-            }]
-        }, {
-            featureType: "administrative.locality",
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#909090"
-            }]
-        }, {
-            featureType: "poi",
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#dbfb7e"
-            }]
-        }, {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [{
-                color: "#414242"
-            }]
-        }, {
-            featureType: "poi.park",
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#909090"
-            }]
-        }, {
-            featureType: "road",
-            elementType: "geometry",
-            stylers: [{
-                color: "#bdd96b"
-            }]
-        }, {
-            featureType: "road",
-            elementType: "geometry.stroke",
-            stylers: [{
-                color: "#849457"
-            }]
-        }, {
-            featureType: "road",
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#eefbc8"
-            }]
-        }, {
-            featureType: "road.highway",
-            elementType: "geometry",
-            stylers: [{
-                color: "#364605"
-            }]
-        }, {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [{
-                color: "#ababab"
-            }]
-        }, {
-            featureType: "road.highway",
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#ebfdb7"
-            }]
-        }, {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [{
-                color: "#777777"
-            }]
-        }, {
-            featureType: "transit.station",
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#6b6b6b"
-            }]
-        }, {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [{
-                color: "#414d54"
-            }]
-        }, {
-            featureType: "water",
-            elementType: "labels.text.fill",
-            stylers: [{
-                color: "#6b6b6b"
-            }]
-        }, {
-            featureType: "water",
-            elementType: "labels.text.stroke",
-            stylers: [{
-                color: "#272727"
-            }]
-        }]
-    });
-    new google.maps.Marker({
-        position: {
-            lat: 47.285339,
-            lng: 9.644274
-        },
-        map: e,
-        title: "Friseur",
-        icon: "img/map/blank.png"
-    })
-}
 $(window).on("load", function() {
     "use strict";
     $(".loading-overlay").fadeOut(1e3)
@@ -176,15 +51,18 @@ $(function() {
         $("html,body").animate({
             scrollTop: 0
         }, 2e3)
-    }),
-    $(".datepicker").datepicker(),
-    new Instafeed({
-        get: "user",
-        userId: "7439059566",
-        limit: 12,
-        resolution: "standard_resolution",
-        accessToken: "7439059566.1677ed0.a668bf15a2fb4f72ac40b0ea975e97d4",
-        sortBy: "most-recent",
-        template: '<div class="col-lg-4 col-xs-4 gallery"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
-    }).run()
+    })    
 });
+
+$(document).ready(function(){
+    var feed = new Instafeed({
+        get: "user",
+        userId: "2257867146",
+        limit: 8,
+        resolution: "standard_resolution",
+        accessToken: "2257867146.1677ed0.94bc3eb2aa4b43129cb8768584c4872d",
+        template: '<div class="col-lg-3 col-xs-12 gallery"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
+    });
+    feed.run();
+});
+
